1c8d1430c2ad99975ef394d4570e0d93b876a743
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-09-10 20:12:06 +0100

    - Added key 'provides' to the Makefile.PL script.

--- lib/Calendar/Hijri.pm
+++ lib/Calendar/Hijri.pm
@@ -1,6 +1,6 @@
 package Calendar::Hijri;
 
-$Calendar::Hijri::VERSION   = '0.25';
+$Calendar::Hijri::VERSION   = '0.26';
 $Calendar::Hijri::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Calendar::Hijri - Interface to Islamic Calendar.
 
 =head1 VERSION
 
-Version 0.25
+Version 0.26
 
 =cut
 
